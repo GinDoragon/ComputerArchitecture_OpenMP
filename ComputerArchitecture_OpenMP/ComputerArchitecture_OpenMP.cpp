df8203d407ad1f54723d64443ead7637f98d646d
Doragon\Gin <ramilbagmanov2@yandex.ru>
2025-04-02 21:38:16 +0300

    Reworked lab

--- ComputerArchitecture_OpenMP/ComputerArchitecture_OpenMP.cpp
+++ ComputerArchitecture_OpenMP/ComputerArchitecture_OpenMP.cpp
@@ -1,64 +1,137 @@
-﻿#include <omp.h>
-#include <iostream>
-#include <windows.h>
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
 
-// Функция для демонстрации параллельного цикла с директивой for
-void parallel_for_example() {
-#pragma omp parallel for
-    for (int i = 0; i < 100; i++) {
-        int thread_id = omp_get_thread_num();
-        std::cout << "Thread " << thread_id << " is executing iteration " << i << "\n";
+using namespace std;
+
+// Функция для выполнения вычислительной нагрузки
+void func() {
+    for (int i = 0; i < 50000; i++) {
+        rand();
     }
 }
 
-// Функция для демонстрации использования директив single и critical
-void single_and_critical_example() {
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Устанавливаем количество потоков равным 2
+    omp_set_dynamic(0);
+    omp_set_num_threads(10);
+
+    cout << "Максимальное количество потоков (omp_get_max_threads): "
+        << omp_get_max_threads() << "\n" << endl;
+
+    // 1. Параллельный цикл с подтверждением числа потоков и использованием barrier
 #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
-
 #pragma omp single
         {
-            Sleep(150);
-            std::cout << "This will be printed by only one thread: " << thread_id << "\n";
+            cout << "Параллельная область (только один поток выполняет вывод), число потоков = "
+                << omp_get_num_threads() << "\n" << endl;
         }
 
+#pragma omp for
+        for (int i = 0; i < 10; i++) {
 #pragma omp critical
-        {
-            Sleep(15);
-            std::cout << "Thread " << thread_id << " is in critical section.\n";
+            {
+                cout << "Параллельный for: поток " << omp_get_thread_num()
+                    << " обрабатывает значение " << i << endl;
+                func();
+            }
         }
     }
-}
 
-// Функция для демонстрации использования замков
-void lock_example() {
+    // 2. Параллельные секции
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+            cout << "Параллельная область (sections), число потоков = "
+                << omp_get_num_threads() << "\n" << endl;
+        }
+
+#pragma omp sections
+        {
+#pragma omp section
+            {
+                for (int i = 0; i < 5; i++) {
+#pragma omp critical
+                    {
+                        cout << "Секция 1: поток " << omp_get_thread_num() << endl;
+                        func();
+                    }
+                }
+            }
+
+#pragma omp section
+            {
+                for (int i = 0; i < 5; i++) {
+#pragma omp critical
+                    {
+                        cout << "Секция 2: поток " << omp_get_thread_num() << endl;
+                        func();
+                    }
+                }
+            }
+        }
+    }
+
+#pragma omp barrier
+
+#pragma omp master
+    {
+        cout << "Все потоки достигли barrier. (Выполнено потоком "
+            << omp_get_thread_num() << ")\n" << endl;
+    }
+
+    // 3. Синхронизация потоков с использованием single и critical для заполнения массива
+    const int arrSize = 10;
+    int array[arrSize] = { 0 };
+
+#pragma omp single
+    {
+        cout << "Инициализация массива выполняется потоком "
+            << omp_get_thread_num() << "\n" << endl;
+    }
+
+    for (int i = 0; i < arrSize; i++) 
+#pragma omp critical    
+    {
+        array[i] = omp_get_thread_num();
+        cout << "Поток " << omp_get_thread_num() << " записал array[" << i << "]" << endl;
+        func();
+    }
+
+    cout << "\nГотовый массив: ";
+    for (int i = 0; i < arrSize; i++) {
+        cout << array[i] << " ";
+    }
+    cout << "\n" << endl;
+
+    // 4. Синхронизация на базе замков
+    cout << "Демонстрация синхронизации с использованием замков:" << endl;
+
     omp_lock_t lock;
     omp_init_lock(&lock);
 
 #pragma omp parallel
     {
-        int thread_id = omp_get_thread_num();
+        for (int i = 0; i < 5; i++) {
+            omp_set_lock(&lock);
+            cout << "Поток " << omp_get_thread_num() << " захватил замок (итерация " << i << ")" << endl;
+            func();
+            omp_unset_lock(&lock);
+        }
 
-        omp_set_lock(&lock);
-        std::cout << "Thread " << thread_id << " is in critical section with lock.\n";
-        omp_unset_lock(&lock);
+        if (omp_test_lock(&lock)) {
+            cout << "Поток " << omp_get_thread_num()
+                << " успешно протестировал и захватил замок." << endl;
+            omp_unset_lock(&lock);
+        }
     }
 
     omp_destroy_lock(&lock);
-}
-
-int main() {
-    omp_set_num_threads(10);
-
-    std::cout << "Running parallel for example:\n";
-    parallel_for_example();
-
-    std::cout << "\nRunning single and critical example:\n";
-    single_and_critical_example();
-
-    std::cout << "\nRunning lock example:\n";
-    lock_example();
 
     return 0;
 }
